798d7aca71f3ee9b1cfec383927952285d410a96
Michael Gannon <michael.c.gannon@stonybrook.edu>
2023-09-19 17:07:27 -0400

    Added base_converter.m

---
+++ base_converter.m
@@ -0,0 +1,14 @@
+% Base Converter
+% Michael Gannon, 112558242
+% AMS 595, Fundamentals of Computing
+
+function base_m = base_converter(n, base_n, m);
+base_m = ''
+i = 0;
+while rem(base_n, n^i) < n^i
+    i = i + 1;
+base_n = rem
+while 0 <= j <=i
+    base_n / 
+end
+
